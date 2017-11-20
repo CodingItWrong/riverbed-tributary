88293f07faea9e81b1a5cfd8838b2d32a9ee84e4
Josh Justice <jjustice@bignerdranch.com>
2017-11-20 11:34:53 -0500

    Send URL shared

--- HydrantShare/ShareViewController.swift
+++ HydrantShare/ShareViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Social
+import MobileCoreServices
 
 class ShareViewController: SLComposeServiceViewController {
 
@@ -17,22 +18,31 @@ class ShareViewController: SLComposeServiceViewController {
     }
 
     override func didSelectPost() {
-        // This is called after the user selects Post. Do the upload of contentText and/or NSExtensionContext attachments.
-        let urlString = "http://localhost:3000/webhooks/hydrant"
-        let url = URL(string: urlString)!
-        let session = URLSession.shared
-        var request = URLRequest(url: url)
-        let bodyDict = ["message": "hello, world!"]
-        let bodyData = try! JSONSerialization.data(withJSONObject: bodyDict, options: [])
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.httpMethod = "POST";
-        request.httpBody = bodyData;
-
-        let task = session.dataTask(with: request)
-        task.resume()
-    
-        // Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
-        self.extensionContext!.completeRequest(returningItems: [], completionHandler: nil)
+        let item: NSExtensionItem = extensionContext!.inputItems[0] as! NSExtensionItem
+        let attachment = item.attachments![0] as! NSItemProvider
+        attachment.loadItem(forTypeIdentifier: kUTTypeURL as String) { data, error in
+            switch data {
+            case let sharedURL as URL:
+                // This is called after the user selects Post. Do the upload of contentText and/or NSExtensionContext attachments.
+                let webhookURLString = "http://localhost:3000/webhooks/hydrant"
+                let webhookURL = URL(string: webhookURLString)!
+                let session = URLSession.shared
+                var request = URLRequest(url: webhookURL)
+                let bodyDict = ["url": sharedURL.absoluteString]
+                let bodyData = try! JSONSerialization.data(withJSONObject: bodyDict, options: [])
+                request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+                request.httpMethod = "POST";
+                request.httpBody = bodyData;
+                
+                let task = session.dataTask(with: request)
+                task.resume()
+                
+                // Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
+                self.extensionContext!.completeRequest(returningItems: [], completionHandler: nil)
+            default:
+                NSLog("no url found")
+            }
+        }
     }
 
     override func configurationItems() -> [Any]! {
